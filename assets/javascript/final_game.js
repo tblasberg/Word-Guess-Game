ddef5a71455f79ef1e0874677e6e3cba7404430b
Terena <46880449+tblasberg@users.noreply.github.com>
2019-03-11 13:39:00 -0400

    Create final_game.js

---
+++ assets/javascript/final_game.js
@@ -0,0 +1,11 @@
+const authors = ["agatha", "stephen", "monteiro", "clarice", "lewis"];
+
+//Choose randomly one author from the array
+let author = authors[Math.floor(Math.random() * authors.length)];
+console.log(author); 
+
+//Capture input from user from text box
+var userInput = document.getElementById('user_guess').value;
+console.log(userInput);
+
+//Iterate over the 
