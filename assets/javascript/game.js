dfa0b9c246cdb9fb6c8f3816404059ff6cd50887
tblasberg <terena.blasberg@gmail.com>
2019-03-13 00:00:18 -0400

    some more changes to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,11 +43,12 @@ function startGame()
     secretWord = authors[Math.floor(Math.random() * authors.length)];
     createSecretArray(secretWord);
     getUserInput(guesses);
+    check(guesses,secretWord);
 }
 //-------------------------------------------------------------
 //Choose randomly one author from the array
 
-//create a fx takes in a secret word and converts to blank
+//create a fx takes in a secret word and converts to blank - DONE
 function createSecretArray(author)
 {
     underscores=[];
@@ -62,13 +63,11 @@ function createSecretArray(author)
             underscores.push("_");
         }
     }
-    //remember to remove the commas so you just get blanks
+    //remember to remove the commas so you just get blanks - DONE   
     //console.log(underscores);
     document.querySelector(".underscores").innerHTML = underscores.join("");
 
     return underscores;
-
-
 }
 
 
@@ -84,19 +83,20 @@ function getUserInput(){
             userInput = event.key;    
         
         
-        console.log("onkey function userinput: " + userInput);
+        console.log("This was the userinput: " + userInput);
         //if stmt saying if we are at the end of the game, do everything to restart,
         //check numGuesses<numTries
         //call the startGame() fx 
         //else run the check fx
-            if(numGuesses >= 10){
+            if(numGuesses >= numTries){
                 alert("Game over!");
                 gameReady=false;
             }
             else { 
                 check(userInput,secretWord)
                 document.querySelector("#user_guess").innerHTML = "What is your guess? "+event.key;}
-            //    console.log("on key event for userinput into array called GUESSES: " + guesses);
+                guesses.push(userInput);
+             console.log("This is my 'guesses' array: " + guesses);
         }
     }
 }
@@ -108,16 +108,16 @@ function getUserInput(){
 //this is a function called "check" and it passes in the userInput to see if it is a wrong guess or right guess.
 //if correct, add to the rightGuesses array
 //if wrong add to the wrongGuesses array
-function check(usersinput,secretWord)
+function check(usersInput,secretWord)
 {
-
-    console.log("check function guesses: " + usersinput + " secret word: "+secretWord);
+    console.log("This is a user's guess: " + usersInput + " and the secret word: "+secretWord);
     console.log("going into the check function")
     //so here you have the user input and secret word, you need to run a loop to check if the 
     //userinput matches the secret word
+    
     guesses.forEach(Element => 
     {
-        if(guesses === author.charAt(usersinput))
+        if(guesses === secretWord.charAt(usersInput.length-1))//compare the guesses' string to the secretWord 
         {
             console.log("The letter " + guesses + " is in the name!");
             rightGuesses.push(guesses);
